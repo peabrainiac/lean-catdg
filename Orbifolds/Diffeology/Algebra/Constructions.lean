0419ce4492c5c89f82e47d7cf6b393d1b9c272b9
Ben Eltschig <b.eltschig@protonmail.com>
2024-05-24 20:59:15 +0200

    feat: diffeologically smooth actions

---
+++ Orbifolds/Diffeology/Algebra/Constructions.lean
@@ -0,0 +1,77 @@
+import Orbifolds.Diffeology.Constructions
+import Orbifolds.Diffeology.DDiffeomorph
+
+/-!
+# Diffeological space structure on the opposite monoid and on the units group
+
+Defines the `DiffeologicalSpace` structure on `Mᵐᵒᵖ`, `Mᵃᵒᵖ`, `Mˣ`, and `AddUnits M`.
+Adapted from `Mathlib.Topology.Algebra.Constructions`
+-/
+
+set_option autoImplicit false
+
+variable {M X : Type*} [dM : DiffeologicalSpace M]
+
+namespace MulOpposite
+
+/-- The opposite monoid is equipped with the same diffeology as the original. -/
+@[to_additive "The opposite monoid is equipped with the same diffeology as the original."]
+instance instDiffeologicalSpaceMulOpposite : DiffeologicalSpace Mᵐᵒᵖ :=
+  dM.induced (unop : Mᵐᵒᵖ → M)
+
+@[to_additive]
+theorem dsmooth_unop : DSmooth (unop : Mᵐᵒᵖ → M) :=
+  dsmooth_induced_dom
+
+@[to_additive]
+theorem dsmooth_op : DSmooth (op : M → Mᵐᵒᵖ) :=
+  dsmooth_induced_rng.2 dsmooth_id
+
+/-- `MulOpposite.op` as a diffeomorphism. -/
+@[to_additive (attr := simps!) "`AddOpposite.op` as a diffeomorphism."]
+def opDiffeomorph : M ᵈ≃ Mᵐᵒᵖ where
+  toEquiv := opEquiv
+  dsmooth_toFun := dsmooth_op
+  dsmooth_invFun := dsmooth_unop
+
+end MulOpposite
+
+namespace Units
+
+open MulOpposite
+
+variable [Monoid M] [DiffeologicalSpace X]
+
+/-- The units of a monoid are equipped with the diffeology induced by the embedding
+  into `M × M`. -/
+@[to_additive "The units of a monoid are equipped with the diffeology induced by the embedding
+into `M × M`."]
+instance instDiffeologicalSpaceUnits : DiffeologicalSpace Mˣ :=
+  DiffeologicalSpace.induced (embedProduct M) inferInstance
+
+@[to_additive]
+theorem induction_embedProduct : Induction (embedProduct M) :=
+  ⟨embedProduct_injective _,rfl⟩
+
+@[to_additive] lemma diffeology_eq_inf : instDiffeologicalSpaceUnits =
+    dM.induced (val : Mˣ → M) ⊓ dM.induced (fun u ↦ ↑u⁻¹ : Mˣ → M) :=
+  induction_embedProduct.2
+
+@[to_additive]
+theorem dsmooth_embedProduct : DSmooth (embedProduct M) :=
+  dsmooth_induced_dom
+
+@[to_additive]
+theorem dsmooth_val : DSmooth ((↑) : Mˣ → M) :=
+  (@dsmooth_embedProduct M _ _).fst
+
+@[to_additive]
+protected theorem dsmooth_iff {f : X → Mˣ} :
+    DSmooth f ↔ DSmooth (val ∘ f) ∧ DSmooth (fun x => ↑(f x)⁻¹ : X → M) := by
+  simp_rw [induction_embedProduct.dsmooth_iff, (· ∘ ·), dsmooth_prod_mk]; rfl
+
+@[to_additive]
+theorem dsmooth_coe_inv : DSmooth (fun u => ↑u⁻¹ : Mˣ → M) :=
+  (Units.dsmooth_iff.1 dsmooth_id).2
+
+end Units
---
+++ Orbifolds/Diffeology/Algebra/MulAction.lean
@@ -0,0 +1,121 @@
+import Orbifolds.Diffeology.Algebra.Constructions
+import Mathlib.Topology.Algebra.MulAction
+
+/-!
+# Smooth actions by monoids
+Adapted from `Mathlib.Topology.Algebra.MulAction`.
+For now this just contains a few basic definitions and lemmas, used to build up the theory of
+diffeological groups and vector spaces.
+-/
+
+set_option autoImplicit false
+
+/-- Class `ContinuousVAdd M X` saying that the additive action `(+ᵥ) : M → X → X`
+  is smooth in both arguments. -/
+class DSmoothVAdd (M X : Type*) [VAdd M X] [DiffeologicalSpace M] [DiffeologicalSpace X] :
+    Prop where
+  dsmooth_vadd : DSmooth fun p : M × X => p.1 +ᵥ p.2
+
+export DSmoothVAdd (dsmooth_vadd)
+
+/-- Class `DSmoothSMul M X` saying that the scalar multiplication `(•) : M → X → X`
+  is smooth in both arguments. -/
+@[to_additive]
+class DSmoothSMul (M X : Type*) [SMul M X] [DiffeologicalSpace M] [DiffeologicalSpace X] :
+    Prop where
+  dsmooth_smul : DSmooth fun p : M × X => p.1 • p.2
+
+export DSmoothSMul (dsmooth_smul)
+
+variable {M X Y : Type*} [DiffeologicalSpace M] [dX : DiffeologicalSpace X]
+  [dY : DiffeologicalSpace Y]
+
+@[to_additive (attr := fun_prop)]
+theorem DSmooth.smul [SMul M X] [DSmoothSMul M X] {f : Y → M} {g : Y → X}
+    (hf : DSmooth f) (hg : DSmooth g) : DSmooth fun x => f x • g x :=
+  dsmooth_smul.comp (hf.prod_mk hg)
+
+@[to_additive]
+instance DSmoothSMul.op [SMul M X] [DSmoothSMul M X] [SMul Mᵐᵒᵖ X] [IsCentralScalar M X] :
+    DSmoothSMul Mᵐᵒᵖ X := ⟨by
+  suffices DSmooth fun p : M × X => MulOpposite.op p.fst • p.snd from
+    this.comp (MulOpposite.dsmooth_unop.prod_map dsmooth_id)
+  simpa only [op_smul_eq_smul] using (dsmooth_smul : DSmooth fun p : M × X => _)⟩
+
+@[to_additive]
+instance MulOpposite.dsmoothSMul [SMul M X] [DSmoothSMul M X] : DSmoothSMul M Xᵐᵒᵖ :=
+  ⟨MulOpposite.dsmooth_op.comp <|
+      dsmooth_smul.comp <| dsmooth_id.prod_map MulOpposite.dsmooth_unop⟩
+
+/-- For any action homomorphism where the action on the codomain is smooth, the induced
+  diffeology makes the action on the domain smooth too.
+  TODO: weaken assumption from `Induction g` (which includes injectivity of `g`) to
+  just `dY = dX.induced g`. -/
+@[to_additive "For any action homomorphism where the action on the codomain is smooth, the induced
+  diffeology makes the action on the domain smooth too."]
+lemma Induction.dsmoothSMul [SMul M X] [DSmoothSMul M X] {N : Type*} [SMul N Y]
+    [DiffeologicalSpace N] {g : Y → X} {f : N → M} (hg : Induction g) (hf : DSmooth f)
+    (hsmul : ∀ {c x}, g (c • x) = f c • g x) : DSmoothSMul N Y := ⟨by
+  simpa only [hg.dsmooth_iff, Function.comp_def, hsmul]
+    using (hf.comp dsmooth_fst).smul <| hg.dsmooth.comp dsmooth_snd⟩
+
+@[to_additive]
+instance SMulMemClass.dsmoothSMul [SMul M X] [DSmoothSMul M X] {S : Type*} [SetLike S X]
+    [SMulMemClass S M X] (s : S) : DSmoothSMul M s :=
+  induction_subtype_val.dsmoothSMul dsmooth_id rfl
+
+section Monoid
+
+variable [Monoid M] [MulAction M X] [DSmoothSMul M X]
+
+@[to_additive]
+instance Units.dsmoothSMul : DSmoothSMul Mˣ X :=
+  induction_id.dsmoothSMul Units.dsmooth_val rfl
+
+/-- Composing a smooth action with a smooth homomorphism results in a smooth action. -/
+@[to_additive]
+theorem MulAction.dsmoothSMul_compHom {N : Type*} [DiffeologicalSpace N] [Monoid N]
+    {f : N →* M} (hf : DSmooth f) :
+    letI : MulAction N X := MulAction.compHom _ f
+    DSmoothSMul N X := by
+  let _ : MulAction N X := MulAction.compHom _ f
+  exact ⟨(hf.comp dsmooth_fst).smul dsmooth_snd⟩
+
+@[to_additive]
+instance Submonoid.dsmoothSMul {S : Submonoid M} : DSmoothSMul S X :=
+  induction_id.dsmoothSMul dsmooth_subtype_val rfl
+
+end Monoid
+
+instance Subgroup.dsmoothSMul [Group M] [MulAction M X] [DSmoothSMul M X] {S : Subgroup M} :
+    DSmoothSMul S X :=
+  S.toSubmonoid.dsmoothSMul
+
+@[to_additive]
+instance Prod.dsmoothSMul [SMul M X] [SMul M Y] [DSmoothSMul M X] [DSmoothSMul M Y] :
+    DSmoothSMul M (X × Y) :=
+  ⟨(dsmooth_fst.smul (dsmooth_fst.comp dsmooth_snd)).prod_mk
+      (dsmooth_fst.smul (dsmooth_snd.comp dsmooth_snd))⟩
+
+@[to_additive]
+instance {ι : Type*} {γ : ι → Type*} [∀ i, DiffeologicalSpace (γ i)] [∀ i, SMul M (γ i)]
+    [∀ i, DSmoothSMul M (γ i)] : DSmoothSMul M (∀ i, γ i) :=
+  ⟨dsmooth_pi fun i => (dsmooth_fst.smul dsmooth_snd).comp <|
+    dsmooth_fst.prod_mk ((dsmooth_apply i).comp dsmooth_snd)⟩
+
+@[to_additive]
+theorem dsmoothSMul_sInf {X : Type*} [SMul M X] {D : Set (DiffeologicalSpace X)}
+    (h : ∀ d ∈ D, @DSmoothSMul M X _ _ d) : @DSmoothSMul M X _ _ (sInf D) :=
+  @DSmoothSMul.mk M X _ _ (_) <| @sInf_singleton _ _ ‹DiffeologicalSpace M› ▸
+    dsmooth_sInf_rng.2 fun t ht => dsmooth_sInf_dom₂ rfl ht
+      (@DSmoothSMul.dsmooth_smul _ _ _ _ t (h t ht))
+
+@[to_additive]
+theorem dsmoothSMul_iInf {X ι : Type*} [SMul M X] {D : ι → DiffeologicalSpace X}
+    (h : ∀ i, @DSmoothSMul M X _ _ (D i)) : @DSmoothSMul M X _ _ (⨅ i, D i) :=
+  dsmoothSMul_sInf <| Set.forall_mem_range.mpr h
+
+@[to_additive]
+theorem dsmoothSMul_inf {X : Type*} [SMul M X] {d₁ d₂ : DiffeologicalSpace X}
+    [@DSmoothSMul M X _ _ d₁] [@DSmoothSMul M X _ _ d₂] : @DSmoothSMul M X _ _ (d₁ ⊓ d₂) :=
+  inf_eq_iInf d₁ d₂ ▸ dsmoothSMul_iInf fun b => (by cases b <;> assumption)
