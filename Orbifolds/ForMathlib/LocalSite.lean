a6288ec1c2e4a5689f8121d8001c29e7ffb43022
Ben Eltschig <b.eltschig@protonmail.com>
2025-03-08 04:57:15 +0100

    feat: sheaves on local sites form local topoi

--- Orbifolds/ForMathlib/LocalSite.lean
+++ Orbifolds/ForMathlib/LocalSite.lean
@@ -1,4 +1,5 @@
 import Orbifolds.ForMathlib.GlobalSections
+import Mathlib.CategoryTheory.Adjunction.Triple
 
 /-!
 # Local sites
@@ -7,6 +8,16 @@ this makes it possible to define codiscrete sheaves on it, giving its sheaf topo
 of a local topos. This makes them an important stepping stone to cohesive sites.
 
 See https://ncatlab.org/nlab/show/local+site.
+
+## Main definitions / results
+* `LocalSite J`: typeclass stating that (C,J) is a local site.
+* `Sheaf.codisc`: the codiscrete functor `Type w ⥤ Sheaf J (Type max v w)` defined on any
+  local site.
+* `Sheaf.ΓCodiscAdj`: the adjunction between the global sections functor `Γ` and `Sheaf.codisc`.
+* `Sheaf.fullyFaithfulCodisc`: `Sheaf.codisc` is fully faithful.
+* `fullyFaithfulConstantSheaf`: on local sites, the constant sheaf functor is fully faithful.
+All together this shows that for local sites `Sheaf J (Type max u v w)` forms a local topos, but
+since we don't yet have local topoi this can't be stated yet.
 -/
 
 universe u v w
@@ -40,25 +51,23 @@ noncomputable def Presheaf.codisc {C : Type u} [Category.{v} C] [HasTerminal C] 
     (coyoneda.obj (op (⊤_ C)) ⋙ uliftFunctor).op
   --curry.obj <| uliftFunctor.prod (coyoneda.obj (op (⊤_ C)) ⋙ uliftFunctor).op ⋙ (uncurry.obj <| yoneda)
 
-noncomputable example [HasTerminal C] : Type max v w ⥤ (Cᵒᵖ ⥤ Type max v w) :=
-  Presheaf.codisc.{u,v,max v w} (C := C)
+/-- On local sites, `Presheaf.codisc` actually takes values in sheaves. -/
+lemma Presheaf.codisc_isSheaf [LocalSite J] (X : Type w) : IsSheaf J (codisc.obj X) := by
+  refine (isSheaf_iff_isSheaf_of_type J _).2 fun Y S hS f hf ↦ ?_
+  refine ⟨fun g ↦ f g.down (LocalSite.from_terminal_mem_of_mem J g.down hS) ⟨𝟙 _⟩, ?_, ?_⟩
+  · intro Z g hg
+    exact funext fun (x : ULift (_ ⟶ _)) ↦
+      (congrFun (f.comp_of_compatible S hf hg x.down) _).trans (congrArg (f g hg) <| by simp)
+  · intro g hg
+    exact funext fun h : ULift (⊤_ C ⟶ Y) ↦ Eq.trans (by simp [Presheaf.codisc]) <|
+      congrFun (hg h.down ((LocalSite.from_terminal_mem_of_mem J h.down hS))) _
 
 /-- The right adjoint to the global sections functor that exists over any local site.
 Takes a type `X` to the sheaf that sends each `Y : C` to the type of functions `Y → X`. -/
 noncomputable def Sheaf.codisc [LocalSite J] :
     Type w ⥤ Sheaf J (Type (max v w)) where
-  obj X := {
-    val := Presheaf.codisc.obj X
-    cond := (isSheaf_iff_isSheaf_of_type J _).2 fun Y S hS f hf ↦ by
-      refine ⟨fun g ↦ f g.down (LocalSite.from_terminal_mem_of_mem J g.down hS) ⟨𝟙 _⟩, ?_, ?_⟩
-      · intro Z g hg
-        exact funext fun (x : ULift (_ ⟶ _)) ↦
-          (congrFun (f.comp_of_compatible S hf hg x.down) _).trans (congrArg (f g hg) <| by simp)
-      · intro g hg
-        exact funext fun h : ULift (⊤_ C ⟶ Y) ↦ Eq.trans (by simp [Presheaf.codisc]) <|
-          congrFun (hg h.down ((LocalSite.from_terminal_mem_of_mem J h.down hS))) _
-  }
-  map {X Y} (f : X → Y) := ⟨Presheaf.codisc.map f⟩
+  obj X := ⟨Presheaf.codisc.obj X, Presheaf.codisc_isSheaf J X⟩
+  map f := ⟨Presheaf.codisc.map f⟩
   map_id _ := rfl
   map_comp _ _ := rfl
 
@@ -91,5 +100,42 @@ instance [LocalSite J] : (Γ J (Type max u v w)).IsLeftAdjoint :=
 
 instance [LocalSite J] : (codisc.{u,v,max u v w} J).IsRightAdjoint :=
   ⟨Γ J _, ⟨ΓCodiscAdj J⟩⟩
+
+/-- The global sections of the codiscrete sheaf on a type are naturally isomorphic to that type.-/
+noncomputable def Sheaf.codiscΓNatIsoId [LocalSite J] :
+    codisc J ⋙ Γ J _ ≅ 𝟭 (Type max u v w) := by
+  refine (isoWhiskerLeft _ (ΓNatIsoSheafSections J _ terminalIsTerminal)).trans ?_
+  exact (NatIso.ofComponents (fun X ↦ {
+    hom x := fun _ ↦ ⟨x⟩
+    inv f := (f (default : ULift (⊤_ C ⟶ ⊤_ C))).down
+    inv_hom_id := by
+      dsimp [codisc, Presheaf.codisc]; ext; dsimp
+      congr; exact Subsingleton.elim _ _
+  })).symm
+
+/-- `Sheaf.codisc` is fully faithful. -/
+noncomputable def Sheaf.fullyFaithfulCodisc [LocalSite J] :
+    (codisc.{u,v,max u v w} J).FullyFaithful :=
+  (ΓCodiscAdj J).fullyFaithfulROfCompIsoId (codiscΓNatIsoId J)
+
+instance [LocalSite J] : (codisc.{u,v,max u v w} J).Full :=
+  (fullyFaithfulCodisc J).full
+
+instance [LocalSite J] : (codisc.{u,v,max u v w} J).Faithful :=
+  (fullyFaithfulCodisc J).faithful
+
+/-- On local sites, the constant sheaf functor is fully faithful. -/
+noncomputable def fullyFaithfulConstantSheaf [HasWeakSheafify J (Type max u v w)] [LocalSite J] :
+    (constantSheaf J (Type max u v w)).FullyFaithful :=
+  ((constantSheafΓAdj J _).fullyFaithfulEquiv (Sheaf.ΓCodiscAdj J)).symm <|
+    Sheaf.fullyFaithfulCodisc J
+
+instance [HasWeakSheafify J (Type max u v w)] [LocalSite J] :
+    (constantSheaf J (Type max u v w)).Full :=
+  (fullyFaithfulConstantSheaf J).full
+
+instance [HasWeakSheafify J (Type max u v w)] [LocalSite J] :
+    (constantSheaf J (Type max u v w)).Faithful :=
+  (fullyFaithfulConstantSheaf J).faithful
 
 end CategoryTheory
